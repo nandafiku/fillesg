3ae2cca1325edf4833aed55b861006bb7341cc08
etdyjsstyhts <la.s.nerhas.t.he.n@gmail.com>
2023-07-12 19:59:03 +0000

    run.sh created online with Bitbucket

---
+++ run.sh
@@ -0,0 +1,96 @@
+gcloud ai hp-tuning-jobs create \
+    --region=us-central1 \
+    --display-name=tes \
+    --max-trial-count=9000 \
+    --parallel-trial-count=10 \
+    --config=config.yaml
+gcloud ai hp-tuning-jobs create \
+    --region=us-east1 \
+    --display-name=tes2 \
+    --max-trial-count=9000 \
+    --parallel-trial-count=10 \
+    --config=config.yaml
+gcloud ai hp-tuning-jobs create \
+    --region=us-east4 \
+    --display-name=tes3 \
+    --max-trial-count=9000 \
+    --parallel-trial-count=10 \
+    --config=config.yaml
+gcloud ai hp-tuning-jobs create \
+    --region=us-west1 \
+    --display-name=tes4 \
+    --max-trial-count=9000 \
+    --parallel-trial-count=10 \
+    --config=config.yaml
+gcloud ai hp-tuning-jobs create \
+    --region=us-west2 \
+    --display-name=tes5 \
+    --max-trial-count=9000 \
+    --parallel-trial-count=10 \
+    --config=config.yaml
+gcloud ai hp-tuning-jobs create \
+    --region=us-west4 \
+    --display-name=tes6 \
+    --max-trial-count=9000 \
+    --parallel-trial-count=10 \
+    --config=config.yaml
+gcloud ai hp-tuning-jobs create \
+    --region=asia-east1 \
+    --display-name=tes7 \
+    --max-trial-count=9000 \
+    --parallel-trial-count=10 \
+    --config=config.yaml
+gcloud ai hp-tuning-jobs create \
+    --region=asia-northeast1 \
+    --display-name=tes8 \
+    --max-trial-count=9000 \
+    --parallel-trial-count=10 \
+    --config=config.yaml
+gcloud ai hp-tuning-jobs create \
+    --region=asia-south1 \
+    --display-name=tes9 \
+    --max-trial-count=9000 \
+    --parallel-trial-count=10 \
+    --config=config.yaml
+gcloud ai hp-tuning-jobs create \
+    --region=asia-southeast1 \
+    --display-name=tes10 \
+    --max-trial-count=9000 \
+    --parallel-trial-count=10 \
+    --config=config.yaml
+gcloud ai hp-tuning-jobs create \
+    --region=australia-southeast1 \
+    --display-name=tes11 \
+    --max-trial-count=9000 \
+    --parallel-trial-count=10 \
+    --config=config.yaml
+gcloud ai hp-tuning-jobs create \
+    --region=europe-central2 \
+    --display-name=tes12 \
+    --max-trial-count=9000 \
+    --parallel-trial-count=10 \
+    --config=config.yaml
+gcloud ai hp-tuning-jobs create \
+    --region=europe-west1 \
+    --display-name=tes13 \
+    --max-trial-count=9000 \
+    --parallel-trial-count=10 \
+    --config=config.yaml
+gcloud ai hp-tuning-jobs create \
+    --region=europe-west2 \
+    --display-name=tes14 \
+    --max-trial-count=9000 \
+    --parallel-trial-count=10 \
+    --config=config.yaml
+gcloud ai hp-tuning-jobs create \
+    --region=europe-west3 \
+    --display-name=tes15 \
+    --max-trial-count=9000 \
+    --parallel-trial-count=10 \
+    --config=config.yaml
+gcloud ai hp-tuning-jobs create \
+    --region=europe-west4\
+    --display-name=tes16 \
+    --max-trial-count=9000 \
+    --parallel-trial-count=10 \
+    --config=config.yaml
